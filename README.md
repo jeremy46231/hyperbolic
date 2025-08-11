a915bf75793e6a825afdba4344ea15b05fea32ef
Jeremy Woolley <jeremy@jeremywoolley.com>
2025-08-11 11:31:38 +0100

    pro plus max ultra seven

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Hyperbolic Pro Plus Max Ultra 7
 
+[https://hyperbolic.jer.app](https://hyperbolic.jer.app)
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 </script>
 
 <svelte:head>
+	<title>Hyperbolic Pro Plus Max Ultra 7</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
