6a94ab6a536184146b6ab5c9c054f2e703f76051
Parneel Bhakhri <pbhakhrism@gmail.com>
2025-08-10 22:48:02 -0400

    add spot challenge

---
+++ src/lib/SpotChallenge.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  const { onNext } = $props();
+
+  let windowObj: Window | null = $state(null);
+  onMount(async () => {
+    windowObj = window;
+  });
+
+  const dotMounted = (dot: HTMLButtonElement) => {
+    const maxX = window.innerWidth - dot.offsetWidth;
+    const maxY = window.innerHeight - dot.offsetHeight;
+
+    // move div to a random place on screen within the viewport
+    dot.style.left = Math.floor(Math.random() * (maxX + 1)) + "px";
+    dot.style.top = Math.floor(Math.random() * (maxY + 1)) + "px";
+  };
+
+  const dotClicked = () => {
+    onNext();
+  };
+</script>
+
+<div class="container">
+  {#if windowObj}
+    <button
+      class="dot"
+      use:dotMounted
+      onclick={dotClicked}
+      aria-label="very secret dot"
+    ></button>
+  {/if}
+</div>
+
+<style>
+  :global(body) {
+    background-color: #3498db;
+  }
+
+  .dot {
+    position: absolute;
+    background: radial-gradient(circle, #4699db 40%, transparent 100%);
+    border: none;
+    width: 60px;
+    height: 60px;
+    margin: 2px;
+    border-radius: 50%;
+  }
+</style>
