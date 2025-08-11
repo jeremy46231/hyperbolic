fb8e147bf54fdd967476b62e00df15ee4bdcfa3a
Jeremy Woolley <jeremy@jeremywoolley.com>
2025-08-10 23:34:56 -0400

    system for advancing through challenges

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
 <script>
   import SpotChallenge from '$lib/SpotChallenge.svelte'
+
+  const challenges = [
+    {
+      component: SpotChallenge,
+    },
+    {
+      component: SpotChallenge,
+    },
+  ]
+
+  let currentChallengeIndex = $state(0)
+  let currentChallenge = $derived(challenges[currentChallengeIndex])
+
+  let done = $state(false)
+
+  const nextChallenge = () => {
+    console.log('next')
+    currentChallengeIndex++
+    if (currentChallengeIndex >= challenges.length) {
+      done = true
+    }
+  }
 </script>
 
-<SpotChallenge onNext={() => {}} />
+{#if !done}
+  {#key currentChallengeIndex}
+    <currentChallenge.component onNext={nextChallenge} />
+  {/key}
+{:else}
+  <div class="done-message">All challenges completed!</div>
+{/if}
